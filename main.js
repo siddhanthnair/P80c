1b96cc59a553b34cca4291996bd1c45c7e9c255b
siddhanthnair <72081663+siddhanthnair@users.noreply.github.com>
2022-04-16 09:35:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function getParagraph1(){
+    var inputs= [];
+    for(var i= 1 ; i<=6 ; i++)
+{
+    
+    var sentence = document.getElementById("para1_input_box_"+ i).value;
+    inputs.push(sentence);
+
+
+}
+var para1 = inputs.join(". ");
+
+document.getElementById("showParagraph1").innerHTML=para1;
+
+}
+
+function getParagraph2(){
+    var inputs= [];
+    for(var i= 1 ; i<=6 ; i++)
+{
+
+    var sentence = document.getElementById("para2_input_box_"+ i).value;
+    console.log(sentence);
+    inputs.push(sentence);
+    console.log(inputs);
+
+
+}
+var para2 = inputs.join(".");
+console.log(para2);
+document.getElementById("showParagraph2").innerHTML=para2;
+
+}
